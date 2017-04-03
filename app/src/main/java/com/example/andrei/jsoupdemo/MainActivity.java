270daa8b71cc962b3a46ed510cf1460ce52c17b0
Yassine MALTI <yassinemalti@gmail.com>
2017-04-03 13:44:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/andrei/jsoupdemo/MainActivity.java
@@ -0,0 +1,172 @@
+package com.example.andrei.jsoupdemo;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+
+public class MainActivity extends Activity {
+
+    String url = "http://lifehacker.com/";
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button titleButton = (Button) findViewById(R.id.titlebutton);
+        Button descButton = (Button) findViewById(R.id.descbutton);
+        Button logoButton = (Button) findViewById(R.id.logobutton);
+
+        titleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Title().execute();
+            }
+        });
+
+        descButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Description().execute();
+            }
+        });
+
+        logoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Logo().execute();
+            }
+        });
+    }
+
+    private class Title extends AsyncTask<Void, Void, Void> {
+        String title;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setTitle("Title");
+            progressDialog.setMessage("Loading...");
+            progressDialog.setIndeterminate(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                Document document = Jsoup.connect(url).get();
+
+                title = document.title();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            TextView txtTitle = (TextView) findViewById(R.id.titletxt);
+            txtTitle.setText(title);
+            progressDialog.dismiss();
+        }
+    }
+
+    private class Description extends AsyncTask<Void, Void, Void> {
+        String desc;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setTitle("Description");
+            progressDialog.setMessage("Loading...");
+            progressDialog.setIndeterminate(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Document document = Jsoup.connect(url).get();
+
+                Elements description = document.select("meta[name=description]");
+                desc = description.attr("content");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            TextView txtDesc = (TextView) findViewById(R.id.desctxt);
+            txtDesc.setText(desc);
+            progressDialog.dismiss();
+        }
+    }
+
+    private class Logo extends AsyncTask<Void, Void, Void> {
+        Bitmap bitmap;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setTitle("Logo");
+            progressDialog.setMessage("Loading...");
+            progressDialog.setIndeterminate(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Document document = Jsoup.connect(url).get();
+
+                Element img = document.select("img").first();
+                String srcValue = img.attr("src");
+                InputStream input = new URL(srcValue).openStream();
+                bitmap = BitmapFactory.decodeStream(input);
+
+            } catch(IOException ex) {
+                ex.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            ImageView logoImg = (ImageView) findViewById(R.id.logo);
+            logoImg.setImageBitmap(bitmap);
+            progressDialog.dismiss();
+        }
+    }
+}
